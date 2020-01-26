a23411e7f68cc927b5083faeb8262c1eb1eb9be4
ivantheratil <ivantheratil@gmail.com>
2020-01-26 00:50:06 -0500

    pic instead

--- mobileApp/App.js
+++ mobileApp/App.js
@@ -3,14 +3,26 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Audio } from 'expo-av';
 import Sound from 'react-native-sound';
 import { playButtonPress } from './audio.js';
+import * as ImagePicker from 'expo-image-picker';
 
 
 export default function App() {
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+  }
   return (
-    <View style={styles.container}>
+    <View style={styles.container}
+    onPress={openImagePickerAsync}>
       <Text>Open up App.js to start working on your app!</Text>
       <TouchableOpacity
-        onPress={playButtonPress}
         style={{ backgroundColor: 'red' }}>
         <Text style={{ fontSize: 20, color: '#fff' }}>Apple</Text>
       </TouchableOpacity>
--- mobileApp/audio.js
+++
@@ -1,8 +0,0 @@
-import Sound from 'react-native-sound';
-
-Sound.setCategory('Ambient', true);
-
-const buttonPress = new Sound(require('./bruh.mp3'), error => console.log(error));
-export const playButtonPress = () => {
-  buttonPress.play((success) => buttonPress.reset());
-}
