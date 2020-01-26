056d21a30252d47592f0761195aae9050e210f5a
ivantheratil <ivantheratil@gmail.com>
2020-01-25 23:18:58 -0500

    Added test button

--- mobileApp/App.js
+++ mobileApp/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={{ backgroundColor: 'red' }}>
+        <Text style={{ fontSize: 20, color: '#fff' }}>Apple</Text>
+      </TouchableOpacity>
     </View>
   );
 }
